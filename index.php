805a264a0b9edcd0560d5786914080d4dd9059ee
choilapitan <choilapitan47@gmail.com>
2020-11-03 11:31:01 +0800

    I added  index with with code in assign_two

--- index.php
+++ index.php
@@ -1,3 +1,20 @@
 <?php
-    echo "Hello World";
+    class User{
+        //PROPERTIES
+        public $name;
+        public $username;
+        public $email;
+
+        function addFriend($name) {
+            echo $name. " added a friend.";
+        }
+        function postStatus($name) {
+            echo $name. " posted a status.";
+        }
+    }
+        //METHOD
+        $example = new User();
+        echo $example->addFriend('Choi');
+        echo "<br>";
+        echo $example->postStatus('Choi');  
 ?>
